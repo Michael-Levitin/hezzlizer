7ada9fd5f8a70f4499a47ffd83e2857a08690b0a
Michael Levitin <stgriard@yandex.ru>
2024-02-28 22:12:35 +0300

    added dto - objects

---
+++ internal/dto/objects.go
@@ -0,0 +1,33 @@
+package dto
+
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
+var (
+	QueryExecuteErorr = fmt.Errorf("could not execute query")
+)
+
+type Meta struct {
+	Total  int `json:"total,omitempty" db:"total"`
+	Remove int `json:"remove,omitempty" db:"remove"`
+	Limit  int `json:"limit,omitempty" db:"limit"`
+	Offset int `json:"offset,omitempty" db:"offset"`
+}
+
+type Goods struct {
+	Id          int       `json:"id" db:"id"`
+	ProjectID   int       `json:"projectId" db:"projectId"`
+	Name        string    `json:"name" db:"name"`
+	Description string    `json:"description" db:"Description"`
+	Priority    int       `json:"priority" db:"priority"`
+	Removed     bool      `json:"removed" db:"removed"`
+	CreatedAt   time.Time `json:"createdAt" db:"createdAt"`
+}
+
+type GetResponse struct {
+	Meta  Meta
+	Goods []Goods
+}
