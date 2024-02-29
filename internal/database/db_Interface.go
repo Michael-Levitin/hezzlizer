8b2e4dbe399cbf4a63a1a0d670eaf6be614e51f6
Michael Levitin <stgriard@yandex.ru>
2024-02-29 11:37:58 +0300

    added db interface

---
+++ internal/database/db_Interface.go
@@ -0,0 +1,14 @@
+package database
+
+import (
+	"context"
+	"github.com/Michael-Levitin/hezzlizer/internal/dto"
+)
+
+type HezzlDbI interface {
+	GoodCreate(ctx context.Context, item *dto.Item) (*dto.Item, error)
+	GoodUpdate(ctx context.Context, item *dto.Item) (*dto.Item, error)
+	GoodRemove(ctx context.Context, info *dto.Item) (*dto.Item, error)
+	GoodsList(ctx context.Context, item *dto.Meta) (*dto.GetResponse, error)
+	GoodReprioritize(ctx context.Context, item *dto.Item) (*dto.ReprResponse, error)
+}
